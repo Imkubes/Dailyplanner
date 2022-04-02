e3865dd202b1ffe048bf782c883bc4daf890f844
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-02 18:44:19 -0500

    Header styled, date centered

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var todaysDateEl = $('.todayDate');
 var dateEl = $('<h1>');
-var dateToday = moment().format('dddd');
+var dateToday = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+dateEl.attr('class', 'dateCenter');
 
 dateEl.text(dateToday);
 
