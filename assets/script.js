5d90f7c41c9ca79a2ebd51de3054f279a8099fa3
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-03 17:48:21 -0500

    Getting closer to boxxes being colored properly

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 //Setting variables and connecting it with html
 var currentHour = moment().hours();
 //console.log(currentHour);
-var container = $('.container');
+var rows = $('.rows');
 var todaysDateEl = $('.todayDate');
 var dateEl = $('<h1>');
 //using the moment api to get today's date
@@ -16,28 +16,43 @@ todaysDateEl.append(dateEl);
 var saveBtn = $('.saveBtn');
 // when user clicks save button, data is saved to local storage
 saveBtn.on('click', function(){
-    var textEnteredEl = $(this).siblings('#dataEntry').val();
+    var textEnteredEl = $(this).siblings('.dataEntry').val();
    // console.log(textEnteredEl);
     var hourKey = $(this).parent().attr('id');
     localStorage.setItem(hourKey, textEnteredEl);
 })
-
-if (currentHour > 9) {
-    
-}
-
 //If it is the next day, clears the local storage inputs to start a blank schedule
 if (currentHour >=23) {
     localStorage.clear();
 } else {
     // Otherwise, display users input for time blocks
-    $("#9 #dataEntry").val(localStorage.getItem("9"));
-    $("#10 #dataEntry").val(localStorage.getItem("10"));
-    $("#11 #dataEntry").val(localStorage.getItem("11"));
-    $("#12 #dataEntry").val(localStorage.getItem("12"));
-    $("#1 #dataEntry").val(localStorage.getItem("1"));
-    $("#2 #dataEntry").val(localStorage.getItem("2"));
-    $("#3 #dataEntry").val(localStorage.getItem("3"));
-    $("#4 #dataEntry").val(localStorage.getItem("4"));
-    $("#5 #dataEntry").val(localStorage.getItem("5"));
+    $("#9 .dataEntry").val(localStorage.getItem("9"));
+    $("#10 .dataEntry").val(localStorage.getItem("10"));
+    $("#11 .dataEntry").val(localStorage.getItem("11"));
+    $("#12 .dataEntry").val(localStorage.getItem("12"));
+    $("#1 .dataEntry").val(localStorage.getItem("1"));
+    $("#2 .dataEntry").val(localStorage.getItem("2"));
+    $("#3 .dataEntry").val(localStorage.getItem("3"));
+    $("#4 .dataEntry").val(localStorage.getItem("4"));
+    $("#5 .dataEntry").val(localStorage.getItem("5"));
 }
+
+function setColor() {
+    var hour = $(this).parent().attr('id');
+
+    if (currentHour > hour) {
+        $('.dataEntry').removeClass('past');
+        $('.dataEntry').removeClass('present');
+        $('.dataEntry').addClass('future');
+    } else if (currentHour == hour) {
+        $('.dataEntry').removeClass('future');
+        $('.dataEntry').removeClass('past');
+        $('.dataEntry').addClass('present');
+    } else {
+        $('.dataEntry').removeClass('present');
+        $('.dataEntry').removeClass('future');
+        $('.dataEntry').addClass('past');
+    }
+}
+
+setColor();
